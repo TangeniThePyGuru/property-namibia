df33147e832d4e6d01cd3ee0a6b3f047eb90f25e
Tangeni <tangenishikomba@gmail.com>
2018-02-09 22:25:06 +0200

    Initial commit by ngx-firebase-bootstrap generator

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+    // Paste all this from the Firebase console...
+    apiKey: "AIzaSyBp8d2gkLrzxVO01kFtANN4Vzj6KspeScU",
+    authDomain: "pwa-bid.firebaseapp.com",
+    databaseURL: "https://pwa-bid.firebaseio.com",
+    storageBucket: "pwa-bid.appspot.com",
+    messagingSenderId: "476863467040"
+};
+

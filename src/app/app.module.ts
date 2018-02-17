3c99e74ef905aff2e1801eb99acf7f47d5e5abe3
Tangeni <tangenishikomba@gmail.com>
2018-02-17 13:57:24 +0200

    terms component skeleton added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,9 @@ import {PropertiesListComponent} from "./properties/properties-list/properties-l
 import {PropertyDetailComponent} from "./properties/property-detail/property-detail.component";
 import {PropertyFormComponent} from "./properties/property-form/property-form.component";
 import { UploadFormComponent } from './properties/upload-form/upload-form.component';
+import { TermsListComponent } from './terms/terms-list/terms-list.component';
+import { TermFormComponent } from './terms/term-form/term-form.component';
+import {TermService} from "./terms/shared/term.service";
 
 const routes: Routes = [
     { path: 'register', component: RegisterPageComponent},
@@ -38,6 +41,8 @@ const routes: Routes = [
     { path: 'properties', component: PropertiesListComponent, canActivate: [LoggedInGuard]},
     { path: 'upload', component: UploadFormComponent, canActivate: [LoggedInGuard]},
     { path: 'properties/create', component: PropertyFormComponent, canActivate: [LoggedInGuard]},
+    { path: 'terms', component: TermsListComponent, canActivate: [LoggedInGuard]},
+    { path: 'terms/create', component: TermFormComponent, canActivate: [LoggedInGuard]},
     { path: '', component: HomePageComponent }
 ];
 
@@ -56,7 +61,9 @@ const routes: Routes = [
         PropertiesListComponent,
         PropertyDetailComponent,
         PropertyFormComponent,
-        UploadFormComponent
+        UploadFormComponent,
+        TermsListComponent,
+        TermFormComponent,
     ],
     imports: [
         BrowserModule,
@@ -68,7 +75,7 @@ const routes: Routes = [
         AngularFireAuthModule,
         RouterModule.forRoot(routes),
     ],
-    providers: [AuthService, LoggedInGuard, PropertyService, UploadService, AngularFireDatabase],
+    providers: [AuthService, LoggedInGuard, PropertyService, UploadService, TermService, AngularFireDatabase],
     bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/terms/terms-list/terms-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-list',
+  templateUrl: './terms-list.component.html',
+  styleUrls: ['./terms-list.component.css']
+})
+export class TermsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

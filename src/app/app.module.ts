055fdf60ac9f0dfbab27ff58c8c27b956f77805d
Tangeni <tangenishikomba@gmail.com>
2018-02-18 03:12:06 +0200

    unused component removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,6 @@ import { DashboardPageComponent } from './pages/dashboard-page.component';
 import {PropertiesListComponent} from "./properties/properties-list/properties-list.component";
 import {PropertyDetailComponent} from "./properties/property-detail/property-detail.component";
 import {PropertyFormComponent} from "./properties/property-form/property-form.component";
-import { UploadFormComponent } from './properties/upload-form/upload-form.component';
 import { TermsListComponent } from './terms/terms-list/terms-list.component';
 import { TermFormComponent } from './terms/term-form/term-form.component';
 import {TermService} from "./terms/shared/term.service";
@@ -40,7 +39,6 @@ const routes: Routes = [
     { path: 'login', component: LoginPageComponent },
     { path: 'dashboard', component: DashboardPageComponent, canActivate: [LoggedInGuard] },
     { path: 'properties', component: PropertiesListComponent, canActivate: [LoggedInGuard]},
-    { path: 'upload', component: UploadFormComponent, canActivate: [LoggedInGuard]},
     { path: 'properties/create', component: PropertyFormComponent, canActivate: [LoggedInGuard]},
     { path: 'terms', component: TermsListComponent, canActivate: [LoggedInGuard]},
     { path: 'terms/create', component: TermFormComponent, canActivate: [LoggedInGuard]},
@@ -62,7 +60,6 @@ const routes: Routes = [
         PropertiesListComponent,
         PropertyDetailComponent,
         PropertyFormComponent,
-        UploadFormComponent,
         TermsListComponent,
         TermFormComponent,
         TermDetailComponent,

e489be60ddfda10d562dfdea2bbb3e9f530ed4fe
Tangeni <tangenishikomba@gmail.com>
2018-02-18 18:15:09 +0200

    ui and ux improved

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,19 +1,22 @@
-import {Component, Output, EventEmitter} from "@angular/core";
+import {Component, Output, EventEmitter, ViewContainerRef} from "@angular/core";
 import {FormGroup, AbstractControl, FormBuilder, Validators} from "@angular/forms";
 import {AuthService} from "app/shared/auth.service";
+import {Toasts} from "../shared/toasts";
+import {ToastsManager} from "ng2-toastr";
 
 @Component({
     selector: 'app-reset-password',
     templateUrl: './reset-password.component.html',
     styleUrls: ['./reset-password.component.css']
 })
-export class ResetPasswordComponent {
+export class ResetPasswordComponent extends Toasts {
     form: FormGroup;
     email: AbstractControl;
     @Output() onSuccess = new EventEmitter();
     @Output() onError = new EventEmitter();
 
-    constructor(private authService: AuthService, private fb: FormBuilder) {
+    constructor(private authService: AuthService, private fb: FormBuilder, public toastr: ToastsManager, vcr: ViewContainerRef) {
+        super(toastr, vcr);
         this.form = fb.group({
             'email': ['', Validators.required]
         });
@@ -27,8 +30,12 @@ export class ResetPasswordComponent {
                     () => {
                         this.onSuccess.emit();
                         this.form.reset();
+                        this.showSuccess('Confirmation Email Successfully sent!, Please check your email.')
                     },
-                    err => this.onError.emit(err)
+                    err => {
+                        this.onError.emit(err);
+                        this.showError('Error, Double Check Your Email and Try Again!');
+                    }
                 );
 
         }

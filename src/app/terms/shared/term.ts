1a033389c54ba341e4c5e6ae09ca90e7a1c259dd
Tangeni <tangenishikomba@gmail.com>
2018-02-25 13:06:05 +0200

    term model constructor added

--- src/app/terms/shared/term.ts
+++ src/app/terms/shared/term.ts
@@ -2,4 +2,9 @@ export class Term {
     $key: string;
     title: string;
     content: string;
+
+    constructor(title, content){
+        this.title = title;
+        this.content = content;
+    }
 }

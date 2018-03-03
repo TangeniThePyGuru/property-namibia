31d1bdc422b8c943ae2355f9166e254ab0b6769d
Tangeni <tangenishikomba@gmail.com>
2018-03-03 17:49:18 +0200

    broker detail component skeleton added

---
+++ src/app/broker/broker-detail/broker-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-broker-detail',
+  templateUrl: './broker-detail.component.html',
+  styleUrls: ['./broker-detail.component.css']
+})
+export class BrokerDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

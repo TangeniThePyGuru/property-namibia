1b0edea847ac5a4acdcf15e37917c7b1800a17ba
Tangeni <tangenishikomba@gmail.com>
2018-03-05 12:52:02 +0200

    ui updated

--- src/app/terms/term-form/term-form.component.ts
+++ src/app/terms/term-form/term-form.component.ts
@@ -4,6 +4,7 @@ import {TermService} from "../shared/term.service";
 import {Toasts} from "../../shared/toasts";
 import {ToastsManager} from "ng2-toastr";
 import {Form} from "@angular/forms";
+import {NgProgress} from "ngx-progressbar";
 
 @Component({
   selector: 'app-term-form',
@@ -15,19 +16,23 @@ export class TermFormComponent extends Toasts implements OnInit{
 
   term: Term;
 
-  constructor(private termSvc: TermService, public toastr: ToastsManager, vcr: ViewContainerRef) {
+  constructor(private termSvc: TermService, public toastr: ToastsManager, vcr: ViewContainerRef,
+              public ngProgress: NgProgress) {
       super(toastr, vcr);
     }
 
   ngOnInit(){}
 
   createTerm(form) {
+      this.ngProgress.start();
       this.term = new Term(form.value.title, form.value.content);
       console.log(form);
       this.termSvc.createTerm(this.term).then(() =>{
+          this.ngProgress.done();
           this.showSuccess('Term Created');
           form.reset();
       }).catch(() =>{
+          this.ngProgress.done();
           this.showError('Error');
       });
   }
--- src/app/terms/term-update-form/term-update-form.component.ts
+++ src/app/terms/term-update-form/term-update-form.component.ts
@@ -1,33 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewContainerRef} from '@angular/core';
 import {Term} from "../shared/term";
 import {ActivatedRoute, Router} from "@angular/router";
 import {TermService} from "../shared/term.service";
+import {NgProgress} from "ngx-progressbar";
+import {Toasts} from "../../shared/toasts";
+import {ToastsManager} from "ng2-toastr";
 
 @Component({
   selector: 'app-term-update-form',
   templateUrl: './term-update-form.component.html',
   styleUrls: ['./term-update-form.component.css']
 })
-export class TermUpdateFormComponent implements OnInit {
+export class TermUpdateFormComponent extends Toasts implements OnInit {
 
   term: Term;
 
-  constructor(private route: ActivatedRoute, private termService: TermService) {
+  constructor(private route: ActivatedRoute, private termService: TermService,
+              public ngProgress: NgProgress, public toastr: ToastsManager, vcr: ViewContainerRef) {
+    super(toastr, vcr);
 
   }
 
   ngOnInit() {
+    this.ngProgress.start();
     this.termService.getTerm(this.route.snapshot.paramMap.get('id')).then((term) => {
       this.term = term;
+      this.ngProgress.done();
       console.log(term);
     });
   }
 
   updateTerm(form) {
-    console.log(form.value);
+    this.ngProgress.start();
     this.termService.updateTerm(this.route.snapshot.paramMap.get('id'), form.value ).then((response)=>{
+      this.ngProgress.done();
+      this.showSuccess('Success, Term Successfully Updated!')
       console.log(response);
     }).catch((error) => {
+      this.ngProgress.done();
+      this.showError('Error, Please Try again!')
       console.log(error);
     });
   }

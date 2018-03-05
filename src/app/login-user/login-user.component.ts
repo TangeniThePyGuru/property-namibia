6268b1cccef7ff3e49197efed258c82713c64734
Tangeni <tangenishikomba@gmail.com>
2018-03-05 13:56:49 +0200

    ui updated

--- src/app/login-user/login-user.component.ts
+++ src/app/login-user/login-user.component.ts
@@ -3,6 +3,7 @@ import {AuthService} from "app/shared/auth.service";
 import {FormBuilder, Validators, AbstractControl, FormGroup} from "@angular/forms";
 import {Toasts} from "../shared/toasts";
 import {ToastsManager} from "ng2-toastr";
+import {NgProgress} from "ngx-progressbar";
 
 @Component({
     selector: 'app-login-user',
@@ -16,7 +17,8 @@ export class LoginUserComponent extends Toasts {
     @Output() onSuccess = new EventEmitter();
     @Output() onError = new EventEmitter();
 
-    constructor(private authService: AuthService, private fb: FormBuilder, public toastr: ToastsManager, vcr: ViewContainerRef) {
+    constructor(private authService: AuthService, private fb: FormBuilder,
+                public toastr: ToastsManager, vcr: ViewContainerRef, public ngProgress: NgProgress) {
         super(toastr, vcr);
         this.form = fb.group({
             'email': ['', Validators.required],
@@ -27,16 +29,19 @@ export class LoginUserComponent extends Toasts {
     }
 
     login() {
+        this.ngProgress.start();
         if (this.form.valid) {
             this.authService.login(this.email.value, this.password.value)
                 .subscribe(
                     () => {
                         this.onSuccess.emit();
                         this.form.reset();
-                        this.showInfo('Login Success');
+                        this.ngProgress.done();
+                        this.showSuccess('Successfully Logged In!');
                     },
                     (err) => {
                         this.onError.emit(err)
+                        this.ngProgress.done();
                         this.showError('Login Error: Please Double Check Your Login Credentials');
                     }
                 );
--- src/app/pages/login-page.component.ts
+++ src/app/pages/login-page.component.ts
@@ -1,18 +1,23 @@
-import {Component} from "@angular/core";
+import {Component, ViewContainerRef} from "@angular/core";
 import {AuthService} from "app/shared/auth.service";
 import { Observable } from "rxjs/Observable";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import {Router} from "@angular/router";
+import {NgProgress} from "ngx-progressbar";
+import {Toasts} from "../shared/toasts";
+import {ToastsManager} from "ng2-toastr";
 
 @Component({
     selector: 'app-login-page',
     templateUrl: './login-page.component.html',
     styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent {
+export class LoginPageComponent extends Toasts{
     isLoggedIn = new BehaviorSubject<boolean>(false);
 
-    constructor(private authService: AuthService, private router: Router) {
+    constructor(private authService: AuthService, private router: Router,
+                public toastr: ToastsManager, vcr: ViewContainerRef) {
+        super(toastr, vcr);
         this.authService.isLoggedIn().subscribe(this.isLoggedIn);
     }
 

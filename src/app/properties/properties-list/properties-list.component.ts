1702c2ee35627c044c503626ea5442518d761ce3
Tangeni <tangenishikomba@gmail.com>
2018-03-05 13:13:01 +0200

    ui updated

--- src/app/properties/properties-list/properties-list.component.ts
+++ src/app/properties/properties-list/properties-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Property} from "../shared/property";
 import { FirebaseListObservable } from "angularfire2/database-deprecated";
 import {PropertyService} from "../shared/property.service";
+import {NgProgress} from "ngx-progressbar";
 
 @Component({
   selector: 'app-properties-list',
@@ -12,14 +13,14 @@ export class PropertiesListComponent implements OnInit {
 
   public properties: FirebaseListObservable<Property[]>;
 
-  constructor(private propertySvc: PropertyService) { }
+  constructor(private propertySvc: PropertyService, public ngProgress: NgProgress) { }
 
   ngOnInit() {
     this.properties = this.propertySvc.getPropertiesList();
   }
 
   deleteProperties() {
-    this.propertySvc.deleteAll()
+    this.propertySvc.deleteAll();
   }
 
 }
